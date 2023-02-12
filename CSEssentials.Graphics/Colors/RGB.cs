af7aeff2841b4bc3793d7d1a0f8a1933b3198e02
NebulaDev <nebuladev.contact@gmail.com>
2023-02-12 21:38:06 +1300

    Add equal comparison for RGB

--- CSEssentials.Graphics/Colors/RGB.cs
+++ CSEssentials.Graphics/Colors/RGB.cs
@@ -59,6 +59,51 @@ public class RGB {
     /// <param name="b">The second color.</param>
     /// <returns>The quotient of the two colors.</returns>
     public static RGB operator /(RGB a, RGB b) => new RGB(a.R / b.R, a.G / b.G, a.B / b.B);
+
+    /// <summary>
+    /// Multiplies an RGB color by a scalar.
+    /// </summary>
+    /// <param name="a">The color.</param>
+    /// <param name="b">The scalar.</param>
+    /// <returns>The product of the color and scalar.</returns>
+    public static RGB operator *(RGB a, int b) => new RGB(a.R * b, a.G * b, a.B * b);
+
+    /// <summary>
+    /// Divides an RGB color by a scalar.
+    /// </summary>
+    /// <param name="a">The color.</param>
+    /// <param name="b">The scalar.</param>
+    /// <returns>The quotient of the color and scalar.</returns>
+    public static RGB operator /(RGB a, int b) => new RGB(a.R / b, a.G / b, a.B / b);
+
+    /// <summary>
+    /// Checks if two RGB colors are equal.
+    /// </summary>
+    /// <param name="a">The first color.</param>
+    /// <param name="b">The second color.</param>
+    /// <returns>True if the colors are equal, false otherwise.</returns>
+    public static bool operator ==(RGB a, RGB b) => a.R == b.R && a.G == b.G && a.B == b.B;
+
+    /// <summary>
+    /// Checks if two RGB colors are not equal.
+    /// </summary>
+    /// <param name="a">The first color.</param>
+    /// <param name="b">The second color.</param>
+    /// <returns>True if the colors are not equal, false otherwise.</returns>
+    public static bool operator !=(RGB a, RGB b) => !(a == b);
+
+    /// <summary>
+    /// Checks if an RGB color is equal to another object.
+    /// </summary>
+    /// <param name="obj">The object to compare to.</param>
+    /// <returns>True if the object is an RGB color and is equal to this color, false otherwise.</returns>
+    public override bool Equals(object? obj) => obj is RGB rgb && this == rgb;
+
+    /// <summary>
+    /// Gets the hash code of this RGB color.
+    /// </summary>
+    /// <returns>The hash code.</returns>
+    public override int GetHashCode() => HashCode.Combine(R, G, B);
     #endregion
 
     #region Functions
@@ -170,6 +215,51 @@ public class RGBA {
     /// <param name="b">The second color.</param>
     /// <returns>The quotient of the two colors.</returns>
     public static RGBA operator /(RGBA a, RGBA b) => new RGBA(a.R / b.R, a.G / b.G, a.B / b.B, a.A / b.A);
+
+    /// <summary>
+    /// Multiplies an RGBA color by a scalar.
+    /// </summary>
+    /// <param name="a">The color.</param>
+    /// <param name="b">The scalar.</param>
+    /// <returns>The product of the color and scalar.</returns>
+    public static RGBA operator *(RGBA a, int b) => new RGBA(a.R * b, a.G * b, a.B * b, a.A * b);
+
+    /// <summary>
+    /// Divides an RGBA color by a scalar.
+    /// </summary>
+    /// <param name="a">The color.</param>
+    /// <param name="b">The scalar.</param>
+    /// <returns>The quotient of the color and scalar.</returns>
+    public static RGBA operator /(RGBA a, int b) => new RGBA(a.R / b, a.G / b, a.B / b, a.A / b);
+
+    /// <summary>
+    /// Checks if two RGBA colors are equal.
+    /// </summary>
+    /// <param name="a">The first color.</param>
+    /// <param name="b">The second color.</param>
+    /// <returns>True if the colors are equal, false otherwise.</returns>
+    public static bool operator ==(RGBA a, RGBA b) => a.R == b.R && a.G == b.G && a.B == b.B && a.A == b.A;
+
+    /// <summary>
+    /// Checks if two RGBA colors are not equal.
+    /// </summary>
+    /// <param name="a">The first color.</param>
+    /// <param name="b">The second color.</param>
+    /// <returns>True if the colors are not equal, false otherwise.</returns>
+    public static bool operator !=(RGBA a, RGBA b) => !(a == b);
+
+    /// <summary>
+    /// Checks if an RGBA color is equal to another object.
+    /// </summary>
+    /// <param name="obj">The object to compare to.</param>
+    /// <returns>True if the object is an RGB color and is equal to this color, false otherwise.</returns>
+    public override bool Equals(object? obj) => obj is RGBA rgb && this == rgb;
+
+    /// <summary>
+    /// Gets the hash code of this RGBA color.
+    /// </summary>
+    /// <returns>The hash code.</returns>
+    public override int GetHashCode() => HashCode.Combine(R, G, B);
     #endregion
 
     #region Functions
@@ -212,7 +302,7 @@ public class RGBA {
     );
 
     /// <summary>
-    /// Fade this RGB color to another, by a specified amount.
+    /// Fade this RGBA color to another, by a specified amount.
     /// </summary>
     /// <param name="to">The color to fade to.</param>
     /// <param name="amount">The amount to fade by.</param>
---
+++ CSEssentials.Tests/GraphicsTests.cs
@@ -0,0 +1,9 @@
+using CSEssentials.Graphics;
+
+namespace CSEssentials.Tests;
+
+public class GraphicsTests
+{
+    [Fact] public void RGBFade() => Assert.Equal(new RGB(255, 255, 255), new RGB(0, 0, 0).Fade(new RGB(255, 255, 255), 1));
+    [Fact] public void RGBHalfLerp() => Assert.Equal(new RGB(127, 127, 127), RGB.Lerp(new RGB(0, 0, 0), new RGB(255, 255, 255), 0.5f));
+}
