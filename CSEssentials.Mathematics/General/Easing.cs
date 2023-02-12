665ce58c61d0c40a73291ca1f465062201857b8b
NebulaDev <nebuladev.contact@gmail.com>
2023-02-12 20:53:15 +1300

    Add initial Vector classes

---
+++ CSEssentials.Mathematics/General/Easing.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace CSEssentials.Mathematics.General;
+
+/// <summary>
+/// A class containing easing functions.
+/// </summary>
+public static class Easing { }
---
+++ CSEssentials.Tests/VectorTests.cs
@@ -0,0 +1,9 @@
+using CSEssentials.Mathematics;
+
+namespace CSEssentials.Tests;
+
+public class VectorTests
+{
+    // [Fact] public void Vector2_Addition() => Assert.Equal(new Vector2(3, 3), new Vector2(1, 1) + new Vector2(2, 2));
+    // [Fact] public void Vector2_Subtraction() => Assert.Equal(new Vector2(-1, -1), new Vector2(1, 1) - new Vector2(2, 2));
+}
